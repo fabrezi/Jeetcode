b5cc539de6154e32eea315f744b49568d9b07817
fabrezi <fkschon@gmail.com>
2024-11-10 08:35:29 -0500

    add: code

--- neetcode/arrays_and_hashing/contains_duplicate.java
+++ neetcode/arrays_and_hashing/contains_duplicate.java
@@ -2,7 +2,27 @@ package arrays_and_hashing;
 
 public class contains_duplicate {
 
+    public static boolean duppy(int[] nums){
+        for(int i=0; i < nums.length; i++){
+            for(int j= i+1; j < nums.length; j++){
+                if(nums[i] == nums[j]){
+                    return(true);
+                }
+
+            }
+        }
+        return false;
+
+
+    }
+
     public static void main(String[] args){
         System.out.println("Hola Cambrone!");
+        int[] nums = {1,2,3,3};
+        int[] bums = {1,2,3,4};
+        int[] lums = {1,3};
+        System.out.println(duppy(nums));
+        System.out.println(duppy(bums));
+        System.out.println(duppy(lums));
     }
 }

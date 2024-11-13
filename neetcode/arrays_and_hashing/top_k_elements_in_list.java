433878140564004e9e260c81e060386a7a791225
fabrezi <fkschon@gmail.com>
2024-11-13 14:22:10 -0500

    ewr

---
+++ neetcode/arrays_and_hashing/top_k_elements_in_list.java
@@ -0,0 +1,18 @@
+package arrays_and_hashing;
+
+public class top_k_elements_in_list {
+
+    public static int[] topKFrequent(int[] nums, int k){
+        for(int i=0; i < nums.length; i++){
+
+        }
+
+    }
+
+    public static void main(String[] args){
+        int [] nums = {1,2,2,3,3,3};
+        int k = 2;
+        System.out.println(topKFrequent(nums, k));
+
+    }
+}

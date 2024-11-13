546a729c69629a3c05b71c4d50a6240d1fb591b4
fabrezi <fkschon@gmail.com>
2024-11-13 06:52:38 -0500

    fasdf

--- neetcode/arrays_and_hashing/is_anagram.java
+++ neetcode/arrays_and_hashing/is_anagram.java
@@ -1,2 +1,37 @@
-package arrays_and_hashing;public class is_anagram {
+package arrays_and_hashing;
+
+import java.util.Arrays;
+
+public class is_anagram {
+
+    public static boolean finder(String s, String t){
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        char[] chars = s.toCharArray();
+        char[] star = t.toCharArray();
+
+        Arrays.sort(chars);
+        Arrays.sort(star);
+
+        if(Arrays.equals(chars, star)){
+            return true;
+        } else{
+            return false;
+        }
+
+
+
+
+
+    }
+
+    public static void main(String[] args){
+        String s = "jar";
+        String t = "raj";
+        System.out.println(finder(s,t));
+
+    }
 }
--- neetcode/sort.java
+++ neetcode/sort.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class sort {
+import java.util.Arrays;
+
+public class sort {
+
+    public static void main(String[] args) {
+
+        String s = "car";
+        String t = "arc";
+
+        char[] chars = s.toCharArray();
+        char[] cahrse = t.toCharArray();
+
+        Arrays.sort(chars);
+        Arrays.sort(cahrse);
+
+        if(Arrays.equals(chars, cahrse)){
+            System.out.println("there is one");
+        }else{
+            System.out.println("WORNG");
+        }
+
+    }
 }
